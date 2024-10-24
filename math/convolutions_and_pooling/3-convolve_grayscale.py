1bbcd19a5f58cbcd6731fd91c339ef590e7fb7fb
M-Pascal <p.mugisha@alustudent.com>
2024-10-24 22:22:24 +0200

    stride_1

--- math/convolutions_and_pooling/3-convolve_grayscale.py
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -10,17 +10,21 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
     Performs a convolution on grayscale images.
 
     Args:
-        images (numpy.ndarray): Shape (m, h, w) containing multiple grayscale images
+        images (numpy.ndarray): Shape (m, h, w)
+        containing multiple grayscale images
             - m: Number of images
             - h: Image height in pixels
             - w: Image width in pixels
-        kernel (numpy.ndarray): Shape (kh, kw) containing the kernel for the convolution
+        kernel (numpy.ndarray): Shape (kh, kw)
+        containing the kernel for the convolution
             - kh: Kernel height
             - kw: Kernel width
         padding (str or tuple): 'same', 'valid', or (ph, pw)
-            - 'same': Zero padding added to ensure the output has the same dimensions as the input.
+            - 'same': Zero padding added to ensure the output
+            has the same dimensions as the input.
             - 'valid': No padding, output will shrink based on kernel size.
-            - (ph, pw): Custom padding for the height (ph) and width (pw) of the image.
+            - (ph, pw): Custom padding for the height (ph) and
+            width (pw) of the image.
         stride (tuple): (sh, sw) strides for height and width
             - sh: Stride along the height of the image
             - sw: Stride along the width of the image
@@ -54,7 +58,8 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
 
     # Apply zero padding to the images
     npad = ((0, 0), (ph, ph), (pw, pw))  # Padding for (height, width)
-    images_padded = np.pad(images, pad_width=npad, mode='constant', constant_values=0)
+    images_padded = np.pad(images, pad_width=npad,
+                           mode='constant', constant_values=0)
 
     # Convolution process: two loops over output image dimensions (nh and nw)
     for i in range(nh):
@@ -63,7 +68,8 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
             y = j * sw  # Start position for width
             # Extract the portion of the padded image that matches the kernel size
             image_slice = images_padded[:, x:x + kh, y:y + kw]
-            # Perform element-wise multiplication between image slice and kernel, and sum the result
+            # Perform element-wise multiplication btn 
+            # image slice and kernel, and sum the result
             convolved[:, i, j] = np.sum(image_slice * kernel, axis=(1, 2))
 
     return convolved
