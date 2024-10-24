d4067a51fc784e2f0e222d7da19e8e33506f64ac
M-Pascal <p.mugisha@alustudent.com>
2024-10-24 22:48:54 +0200

    conv channels

--- math/convolutions_and_pooling/4-convolve_channels.py
+++ math/convolutions_and_pooling/4-convolve_channels.py
@@ -41,7 +41,7 @@ def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
     sh, sw = stride # Stride for height (sh) and width (sw)
     # Determine padding values based on the type of padding specified
     if padding == 'same':
-        # Padding calculation for 'same' to maintain output dimensions same as input
+        # Padding calc for 'same' to maintain output dimensions same as input
         ph = int(((h - 1) * sh + kh - h) / 2) + 1
         pw = int(((w - 1) * sw + kw - w) / 2) + 1
     elif padding == 'valid':
@@ -58,18 +58,21 @@ def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
     # Initialize the output array to store the convolved images
     convolved = np.zeros((m, nh, nw))  # Output shape is (m, nh, nw)
 
-    # Apply padding to the img(only for height and width, channels are left unchanged)
-    npad = ((0, 0), (ph, ph), (pw, pw), (0, 0))  # No padding for m and c dimensions
-    imagesp = np.pad(images, pad_width=npad, mode='constant', constant_values=0)
+    # Apply padding to the img
+    # only for height and width, channels are left unchanged
+    # No padding for m and c dimensions
+    npad = ((0, 0), (ph, ph), (pw, pw), (0, 0))
+    imagesp = np.pad(images, pad_width=npad,
+                     mode='constant', constant_values=0)
 
     # Loop over each pixel of the output to compute the convolution
     for i in range(nh):
         x = i * sh  # Start index for height based on stride
         for j in range(nw):
             y = j * sw  # Start index for width based on stride
-            # Extract the sub-section of the padded img that matches the kernel sz
+            # Extract sub-section of the padded img that matches the kernel sz
             image_slice = imagesp[:, x:x + kh, y:y + kw, :]
-            # Perform element-wise multiplication between the slice 
+            # Perform element-wise multiplication between the slice
             # and kernel, then sum over the spatial dimensions
             convolved[:, i, j] = np.sum(image_slice * kernel, axis=(1, 2, 3))
 
