093e5c33f1b0065582786634a8dab002cc40b015
M-Pascal <p.mugisha@alustudent.com>
2025-01-24 01:57:06 +0200

    deep& one_hot

--- supervised_learning/classification/20-deep_neural_network.py
+++ supervised_learning/classification/20-deep_neural_network.py
@@ -89,7 +89,8 @@ class DeepNeuralNetwork:
             cost (float): the cost of the predictions
         """
         m = Y.shape[1]
-        cost = -(1 / m) * np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))
+        cost = -(1 / m) * np.sum(Y * np.log(A)
+                                 + (1 - Y) * np.log(1.0000001 - A))
         return cost
 
     def evaluate(self, X, Y):
