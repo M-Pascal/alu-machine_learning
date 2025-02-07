93a6195ebd418d3c603cf7ef8689fae920c29437
MPascal <p.mugisha@alustudent.com>
2025-02-07 22:35:54 +0200

    Updates on RMSprop optimizer

--- supervised_learning/optimization/8-RMSProp.py
+++ supervised_learning/optimization/8-RMSProp.py
@@ -12,7 +12,7 @@ def create_RMSProp_op(loss, alpha, beta2, epsilon):
         loss: The loss of the network.
         alpha: The learning rate.
         beta2: The RMSProp decay factor (moving average decay).
-        epsilon: A small number to avoid division by zero.   
+        epsilon: A small number to avoid division by zero.
     Returns:
         The RMSProp optimization operation.
     """
