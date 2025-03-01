cb934232f6148e9e07f2bf6362bf0cad653924d8
M-Pascal <p.mugisha@alustudent.com>
2025-03-02 01:18:05 +0200

    apis updates

--- pipeline/apis/3-upcoming.py
+++ pipeline/apis/3-upcoming.py
@@ -6,24 +6,32 @@ ordered by the number of launches in descending order or,
 if rockets have the same amount of launches, in alphabetical order
 """
 
-
 import requests
-
 
 if __name__ == "__main__":
     url = 'https://api.spacexdata.com/v4/launches'
-    results = requests.get(url).json()
-    rocketDict = {}
-    for launch in results:
-        rocket = launch.get('rocket')
-        url = 'https://api.spacexdata.com/v4/rockets/{}'.format(rocket)
-        results = requests.get(url).json()
-        rocket = results.get('name')
-        if rocketDict.get(rocket) is None:
-            rocketDict[rocket] = 1
-        else:
-            rocketDict[rocket] += 1
-    rocketList = sorted(rocketDict.items(), key=lambda kv: kv[0])
-    rocketList = sorted(rocketList, key=lambda kv: kv[1], reverse=True)
-    for rocket in rocketList:
-        print("{}: {}".format(rocket[0], rocket[1]))
+    launches = requests.get(url).json()
+    
+    rocket_counts = {}
+    
+    for launch in launches:
+        rocket_id = launch.get('rocket')
+        if rocket_id not in rocket_counts:
+            rocket_counts[rocket_id] = 0
+        rocket_counts[rocket_id] += 1
+    
+    # Fetch rocket names
+    rocket_names = {}
+    for rocket_id in rocket_counts.keys():
+        rocket_info = requests.get(f'https://api.spacexdata.com/v4/rockets/{rocket_id}').json()
+        rocket_names[rocket_id] = rocket_info.get('name')
+
+    # Convert to a list of tuples (name, count)
+    rocket_list = [(rocket_names[rid], count) for rid, count in rocket_counts.items()]
+
+    # Sort by number of launches (descending), then by name (ascending)
+    rocket_list.sort(key=lambda x: (-x[1], x[0]))
+
+    # Print results
+    for name, count in rocket_list:
+        print(f"{name}: {count}")
