b03caf75215a39eb76275a4d2feb75df232c1c3b
M-Pascal <p.mugisha@alustudent.com>
2025-06-23 00:24:11 +0200

    clusting

--- unsupervised_learning/clustering/10-kmeans.py
+++ unsupervised_learning/clustering/10-kmeans.py
@@ -1,22 +1,24 @@
 #!/usr/bin/env python3
-"""This module contains a function that perfoms
-performs K-means on a dataset"""
+"""K-means using scikit learn"""
+
 import sklearn.cluster
 
 
 def kmeans(X, k):
     """
-    performs K-means on a dataset:
-    X is a numpy.ndarray of shape (n, d) containing the dataset
-    k is the number of clusters
+    performs K-means on a dataset
+    Args:
+        X: numpy.ndarray of shape (n, d) containing the dataset
+        k: number of clusters
     Returns: C, clss
-        C: is a numpy.ndarray of shape (k, d) containing the centroid means for
-            each cluster
-        clss: is a numpy.ndarray of shape (n,) containing the index of the
-            cluster in C that each data point belongs to
+             C: numpy.ndarray of shape (k, d) containing the centroid
+                means for each cluster
+             clss: numpy.ndarray of shape (n,) containing the index of
+                   the cluster in C that each data point belongs to
     """
-    k_model = sklearn.cluster.KMeans(n_clusters=k).fit(X)
-    clss = k_model.labels_
-    C = k_model.cluster_centers_
+    k_mean = sklearn.cluster.KMeans(n_clusters=k)
+    k_mean.fit(X)
+    clss = k_mean.labels_
+    C = k_mean.cluster_centers_
 
     return C, clss
--- unsupervised_learning/clustering/11-gmm.py
+++ unsupervised_learning/clustering/11-gmm.py
@@ -1,26 +1,32 @@
 #!/usr/bin/env python3
-"""This module contains a function that perfoms
-calculates a GMM from a dataset"""
+""" GMM using scikit learn """
+
 import sklearn.mixture
 
 
 def gmm(X, k):
     """
     calculates a GMM from a dataset
-        X is a numpy.ndarray of shape (n, d) containing the dataset
-        k is the number of clusters
+    Arg:
+        X: np.ndarray of shape (n, d) containing the dataset
+        n: the number of data points
+        d: the number of dimensions for each data point
+        k: positive int containing the number of clusters
     Returns: pi, m, S, clss, bic
-        pi: is a numpy.ndarray of shape (k,) containing the cluster priors
-        m: is a numpy.ndarray of shape (k, d) containing the centroid means
-        S: is a numpy.ndarray of shape (k, d, d) containing the covariance
-            matrices
-        clss: is a numpy.ndarray of shape (n,) containing the cluster indices
-            for each data point
-        bic: is a numpy.ndarray of shape (kmax - kmin + 1) containing the BIC
-            value for each cluster size tested
+             pi: numpy.ndarray of shape (k,) containing the cluster priors.
+             m: numpy.ndarray of shape (k, d) containing the centroid means.
+             S: numpy.ndarray of shape (k, d, d) containing the covariance
+                matrices.
+             clss: numpy.ndarray of shape (n,) containing the cluster indices
+                   for each data point
+             bic: numpy.ndarray of shape (kmax - kmin + 1) containing the BIC
+                  value for each cluster size tested.
     """
-    Gmm = sklearn.mixture.GaussianMixture(k)
-    params = Gmm.fit(X)
-    clss = Gmm.predict(X)
-    return (params.weights_, params.means_,
-            params.covariances_, clss, Gmm.bic(X))
+    g = sklearn.mixture.GaussianMixture(n_components=k)
+    g.fit(X)
+    pi = g.weights_
+    m = g.means_
+    S = g.covariances_
+    clss = g.predict(X)
+    bic = g.bic(X)
+    return pi, m, S, clss, bic
